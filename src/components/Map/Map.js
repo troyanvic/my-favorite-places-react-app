d8f2c65b02732c2e6f6a4f826285cd6bb0fece9d
Troayn Victor <troyanvic@gmail.com>
2019-11-11 09:06:37 +0200

    installed recompose package and created Map component

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,11 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component }                 from 'react';
+import { connect }                          from 'react-redux';
+import { compose, withProps }               from 'recompose';
+import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 
-class Map extends Component{
-    render() {
-        return (
-            <section className="map">Map component</section>
-        )
-    }
+/*
+ * Create map element with HOC
+ *
+ * @return React Component
+ */
+const MapElement = compose(
+  withProps({
+    containerElement: <section className="map" />,
+    mapElement: <div className="map__element" />
+  }),
+  withGoogleMap
+)( props => {
+  let { isMarkerShown, lat, lng } = props;
+
+  return <GoogleMap defaultZoom={ 8 }
+                    defaultCenter={ { lat, lng} }
+                    center={ { lat, lng } }>
+    { isMarkerShown && ( <Marker position={ { lat, lng } } /> ) }
+  </GoogleMap>
+});
+
+class Map extends Component {
+  render() {
+    let { lat, lng } = this.props.currentPlace;
+
+    return <MapElement isMarkerShown lat={ lat } lng={ lng } />;
+  }
 }
 
-export default Map;
+// store
+const mapStateToProps = store => ( { currentPlace: store.currentPoint } );
+
+export default connect(
+  mapStateToProps
+)( Map );
