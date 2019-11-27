cae0c8f0f5eb5c62ef60bb7a09586d34e8d79ae7
Troayn Victor <troyanvic@gmail.com>
2019-11-27 09:58:50 +0200

    added component Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 
 class Footer extends Component {
   render() {
+    const currDate = new Date();
+
     return (
-      <div>
-        footer
-      </div>
+      <section className="footer">
+        <p className="footer__copyright">&copy; <a href="https://github.com/troyanvic" title="Troyan Vic's Github profile" target="_blank">Troyan Vic</a>, { currDate.getFullYear() }</p>
+      </section>
     );
   }
 }
